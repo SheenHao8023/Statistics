0f19832aead4b291d40e452f19fac8bac59dcbae
Xin Hao <126965580+SheenHao8023@users.noreply.github.com>
2024-09-28 21:33:55 +0800

    Update MANOVA-EMMEANS.r

--- MANOVA-EMMEANS.r
+++ MANOVA-EMMEANS.r
@@ -3,140 +3,158 @@ library(bruceR)
 library(dplyr)
 library(writexl)
 
-file_path <- "C:/Users/ASUS/Desktop/WaveletCoherence.xlsx"
+file_path <- "C:/Users/ASUS/Desktop/SCZ_tACS/data/IBS/statistic/IBS.xlsx"
 data <- read_excel(file_path, col_names = TRUE)
-data[data =="NaN"] <- NA
-data$Age_centered <- scale(data$Age, center = TRUE, scale = FALSE)[, , drop = FALSE]
 data <- data %>%
   mutate(across(starts_with("C"), ~ as.numeric(.)))
 data$GMrest <- apply(data[, sprintf("C1FC%02d", 1:66)], MARGIN = 1, FUN = mean, na.rm = TRUE)
 data$GMall <- apply(data[, sprintf("C%dFC%02d", rep(1:5, each = 66), 1:66)], MARGIN = 1, FUN = mean, na.rm = TRUE)
-write_xlsx(data, "C:/Users/ASUS/Desktop/GlobalMean.xlsx")
 
-
-m = MANOVA(data, dvs = "C1FC01:C5FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
-        within = c("Condition", "FunctionalConnectivity"), covariate = c("Gender", "Age_centered","GMrest"),
+m1 = MANOVA(data, dvs = "C1FC01:C5FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
+        within = c("Condition", "IBS"), 
         ss.type = "III", sph.correction = "GG", aov.include = FALSE, digits = 3,
-        file = "C:/Users/ASUS/Desktop/WaveletCoherence1.doc")  %>%
-    EMMEANS("Group",by = c("Condition","FunctionalConnectivity"),
+        file = "C:/Users/ASUS/Desktop/IBS1.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/WaveletCoherence2.doc")   %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
-            contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
-            model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/WaveletCoherence3.doc")   %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+            file = "C:/Users/ASUS/Desktop/IBS2.doc")   %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "fdr",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/WaveletCoherence4.doc") 
-emmip(m, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "Coherence")
-emmip(m, Group ~ Condition | FunctionalConnectivity, CIs=TRUE)
+            file = "C:/Users/ASUS/Desktop/IBS3.doc") 
+emmip(m1, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "IBS")
+emmip(m1, Group ~ Condition | IBS, CIs=TRUE)
 
 m2 = MANOVA(data, dvs = "C1FC01:C5FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
-        within = c("Condition", "FunctionalConnectivity"), covariate = c("Gender", "Age_centered"),
+        within = c("Condition", "IBS"), covariate = "GMrest",
         ss.type = "III", sph.correction = "GG", aov.include = FALSE, digits = 3,
-        file = "C:/Users/ASUS/Desktop/FC1.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+        file = "C:/Users/ASUS/Desktop/IBS4.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/FC2.doc") 
-emmip(m2, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "Coherence")
-emmip(m2, Group ~ Condition | FunctionalConnectivity, CIs=TRUE)
+            file = "C:/Users/ASUS/Desktop/IBS5.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
+            contrast = "pairwise",reverse = TRUE,p.adjust = "fdr",sd.pooled = NULL,
+            model.type = "multivariate",digits = 3,
+            file = "C:/Users/ASUS/Desktop/IBS6.doc") 
+emmip(m2, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "IBS")
+emmip(m2, Group ~ Condition | IBS, CIs=TRUE)
 
-#resting state vs. hearing each other
-file_path <- "C:/Users/ASUS/Desktop/RS-EO.xlsx"
+m3 = MANOVA(data, dvs = "C1FC01:C5FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
+        within = c("Condition", "IBS"), covariate = "GMall",
+        ss.type = "III", sph.correction = "GG", aov.include = FALSE, digits = 3,
+        file = "C:/Users/ASUS/Desktop/IBS7.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
+            contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
+            model.type = "multivariate",digits = 3,
+            file = "C:/Users/ASUS/Desktop/IBS8.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
+            contrast = "pairwise",reverse = TRUE,p.adjust = "fdr",sd.pooled = NULL,
+            model.type = "multivariate",digits = 3,
+            file = "C:/Users/ASUS/Desktop/IBS9.doc") 
+emmip(m3, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "IBS")
+emmip(m3, Group ~ Condition | IBS, CIs=TRUE)
+
+file_path <- "C:/Users/ASUS/Desktop/SCZ_tACS/data/IBS/statistic/RS-EO.xlsx"
 data <- read_excel(file_path, col_names = TRUE)
-data[data =="NaN"] <- NA
-data$Age_centered <- scale(data$Age, center = TRUE, scale = FALSE)[, , drop = FALSE]
 data <- data %>%
   mutate(across(starts_with("C"), ~ as.numeric(.)))
 data$GMrest <- apply(data[, sprintf("C1FC%02d", 1:66)], MARGIN = 1, FUN = mean, na.rm = TRUE)
 data$GMall <- apply(data[, sprintf("C%dFC%02d", rep(1:2, each = 66), 1:66)], MARGIN = 1, FUN = mean, na.rm = TRUE)
 
 m1 = MANOVA(data, dvs = "C1FC01:C2FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
-        within = c("Condition", "FunctionalConnectivity"), covariate = c("Gender", "Age_centered"),
+        within = c("Condition", "IBS"), 
         ss.type = "III", sph.correction = "GG", aov.include = FALSE, digits = 3,
-        file = "C:/Users/ASUS/Desktop/RS_EO1.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+        file = "C:/Users/ASUS/Desktop/RS-EO1.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_EO2.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+            file = "C:/Users/ASUS/Desktop/RS-EO2.doc")   %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "fdr",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_EO3.doc") 
+            file = "C:/Users/ASUS/Desktop/RS-EO3.doc") 
+emmip(m1, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "IBS")
+emmip(m1, Group ~ Condition | IBS, CIs=TRUE)
+
 m2 = MANOVA(data, dvs = "C1FC01:C2FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
-        within = c("Condition", "FunctionalConnectivity"), covariate = c("Gender", "Age_centered", "GMrest"),
+        within = c("Condition", "IBS"), covariate = "GMrest",
         ss.type = "III", sph.correction = "GG", aov.include = FALSE, digits = 3,
-        file = "C:/Users/ASUS/Desktop/RS_EO4.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+        file = "C:/Users/ASUS/Desktop/RS-EO4.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_EO5.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+            file = "C:/Users/ASUS/Desktop/RS-EO5.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "fdr",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_EO6.doc") 
+            file = "C:/Users/ASUS/Desktop/RS-EO6.doc") 
+emmip(m2, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "IBS")
+emmip(m2, Group ~ Condition | IBS, CIs=TRUE)
+
 m3 = MANOVA(data, dvs = "C1FC01:C2FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
-        within = c("Condition", "FunctionalConnectivity"), covariate = c("Gender", "Age_centered", "GMall"),
+        within = c("Condition", "IBS"), covariate = "GMall",
         ss.type = "III", sph.correction = "GG", aov.include = FALSE, digits = 3,
-        file = "C:/Users/ASUS/Desktop/RS_EO7.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+        file = "C:/Users/ASUS/Desktop/RS-EO7.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_EO8.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+            file = "C:/Users/ASUS/Desktop/RS-EO8.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "fdr",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_EO9.doc") 
+            file = "C:/Users/ASUS/Desktop/RS-EO9.doc") 
+emmip(m3, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "IBS")
+emmip(m3, Group ~ Condition | IBS, CIs=TRUE)
 
-
-#resting state vs. tasking state
-file_path <- "C:/Users/ASUS/Desktop/RS-TS.xlsx"
+file_path <- "C:/Users/ASUS/Desktop/SCZ_tACS/data/IBS/statistic/RS-TS.xlsx"
 data <- read_excel(file_path, col_names = TRUE)
-data[data =="NaN"] <- NA
-data$Age_centered <- scale(data$Age, center = TRUE, scale = FALSE)[, , drop = FALSE]
 data <- data %>%
   mutate(across(starts_with("C"), ~ as.numeric(.)))
 data$GMrest <- apply(data[, sprintf("C1FC%02d", 1:66)], MARGIN = 1, FUN = mean, na.rm = TRUE)
 data$GMall <- apply(data[, sprintf("C%dFC%02d", rep(1:2, each = 66), 1:66)], MARGIN = 1, FUN = mean, na.rm = TRUE)
-write_xlsx(data, "C:/Users/ASUS/Desktop/GlobalMean2.xlsx")
-
 
 m1 = MANOVA(data, dvs = "C1FC01:C2FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
-        within = c("Condition", "FunctionalConnectivity"), covariate = c("Gender", "Age_centered"),
+        within = c("Condition", "IBS"), 
         ss.type = "III", sph.correction = "GG", aov.include = FALSE, digits = 3,
-        file = "C:/Users/ASUS/Desktop/RS_TSO1.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+        file = "C:/Users/ASUS/Desktop/RS-TS1.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_TSO2.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+            file = "C:/Users/ASUS/Desktop/RS-TS2.doc")   %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "fdr",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_TSO3.doc") 
+            file = "C:/Users/ASUS/Desktop/RS-TS3.doc") 
+emmip(m1, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "IBS")
+emmip(m1, Group ~ Condition | IBS, CIs=TRUE)
+
 m2 = MANOVA(data, dvs = "C1FC01:C2FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
-        within = c("Condition", "FunctionalConnectivity"), covariate = c("Gender", "Age_centered", "GMrest"),
+        within = c("Condition", "IBS"), covariate = "GMrest",
         ss.type = "III", sph.correction = "GG", aov.include = FALSE, digits = 3,
-        file = "C:/Users/ASUS/Desktop/RS_TSO4.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+        file = "C:/Users/ASUS/Desktop/RS-TS4.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_TSO5.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+            file = "C:/Users/ASUS/Desktop/RS-TS5.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "fdr",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_TSO6.doc") 
+            file = "C:/Users/ASUS/Desktop/RS-TS6.doc") 
+emmip(m2, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "IBS")
+emmip(m2, Group ~ Condition | IBS, CIs=TRUE)
+
 m3 = MANOVA(data, dvs = "C1FC01:C2FC66", dvs.pattern = "C(.)FC(..)", between = "Group",
-        within = c("Condition", "FunctionalConnectivity"), covariate = c("Gender", "Age_centered", "GMall"),
+        within = c("Condition", "IBS"), covariate = "GMall",
         ss.type = "III", sph.correction = "GG", aov.include = FALSE, digits = 3,
-        file = "C:/Users/ASUS/Desktop/RS_TSO7.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+        file = "C:/Users/ASUS/Desktop/RS-TS7.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "bonferroni",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_TSO8.doc")  %>%
-    EMMEANS(c("Group","Condition"), by = "FunctionalConnectivity",
+            file = "C:/Users/ASUS/Desktop/RS-TS8.doc")  %>%
+    EMMEANS(c("Group","Condition"), by = "IBS",
             contrast = "pairwise",reverse = TRUE,p.adjust = "fdr",sd.pooled = NULL,
             model.type = "multivariate",digits = 3,
-            file = "C:/Users/ASUS/Desktop/RS_TSO9.doc") 
+            file = "C:/Users/ASUS/Desktop/RS-TS9.doc") 
+emmip(m3, Group ~ Condition, CIs=TRUE, xlab = "Condition",ylab = "IBS")
+emmip(m3, Group ~ Condition | IBS, CIs=TRUE)

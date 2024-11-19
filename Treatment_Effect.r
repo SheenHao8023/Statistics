a0412f2d7e966faa4b422afb92dab190d0e66f9b
Xin Hao <126965580+SheenHao8023@users.noreply.github.com>
2024-11-19 16:19:16 +0800

    Add files via upload

---
+++ Treatment_Effect.r
@@ -0,0 +1,121 @@
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(ggprism)
+
+data <- read_excel("C:/Users/ASUS/Desktop/FollowUp.xlsx", col_names = TRUE, sheet = 1)
+
+data_condition <- data %>% select(ends_with("C1"))
+data_long <- data_condition %>%
+  pivot_longer(cols = everything(), names_to = "Condition", values_to = "value")
+ggplot(data_long, aes(x = factor(Condition), y = value, fill = Condition))+ 
+  stat_summary(fun = "mean",geom = "bar", width = 0.7, colour = "black", size = 0.5)+ 
+  geom_jitter(size = 2, width = 0.2)+ 
+  stat_summary(fun = "mean", fun.max = function(x) mean(x) + sd(x), fun.min = function(x) mean(x) - sd(x), 
+               geom = "errorbar", width = 0.4, size = 0.8)+ 
+  theme_prism(axis_text_angle = 0)+ 
+  theme(legend.position = 'right',
+    legend.direction = "vertical",
+    axis.text = element_text(size=12, color="black"),  # 轴文本大小和颜色
+    axis.title = element_text(size=14, face="bold"),   # 轴标题大小和样式
+    plot.title = element_text(hjust = 0.5, size=16, face="bold")  ) +
+  coord_cartesian(ylim = c(0, 1)) +
+  labs(x='Hearing each other', y="Consistency") +
+  scale_fill_manual(values = c('#f47c7c', '#f47c7c', '#f47c7c'))
+
+data_condition <- data %>% select(ends_with("C2"))
+data_long <- data_condition %>%
+  pivot_longer(cols = everything(), names_to = "Condition", values_to = "value")
+ggplot(data_long, aes(x = factor(Condition), y = value, fill = Condition))+ 
+  stat_summary(fun = "mean",geom = "bar", width = 0.7, colour = "black", size = 0.5)+ 
+  geom_jitter(size = 2, width = 0.2)+ 
+  stat_summary(fun = "mean", fun.max = function(x) mean(x) + sd(x), fun.min = function(x) mean(x) - sd(x), 
+               geom = "errorbar", width = 0.4, size = 0.8)+ 
+  theme_prism(axis_text_angle = 0)+ 
+  theme(legend.position = 'right',
+    legend.direction = "vertical",
+    axis.text = element_text(size=12, color="black"),  # 轴文本大小和颜色
+    axis.title = element_text(size=14, face="bold"),   # 轴标题大小和样式
+    plot.title = element_text(hjust = 0.5, size=16, face="bold")  ) +
+  coord_cartesian(ylim = c(0, 1)) +
+  labs(x='Hearing A', y="Consistency") +
+  scale_fill_manual(values = c('#f47c7c', '#f47c7c', '#f47c7c'))
+
+data_condition <- data %>% select(ends_with("C3"))
+data_long <- data_condition %>%
+  pivot_longer(cols = everything(), names_to = "Condition", values_to = "value")
+ggplot(data_long, aes(x = factor(Condition), y = value, fill = Condition))+ 
+  stat_summary(fun = "mean",geom = "bar", width = 0.7, colour = "black", size = 0.5)+ 
+  geom_jitter(size = 2, width = 0.2)+ 
+  stat_summary(fun = "mean", fun.max = function(x) mean(x) + sd(x), fun.min = function(x) mean(x) - sd(x), 
+               geom = "errorbar", width = 0.4, size = 0.8)+ 
+  theme_prism(axis_text_angle = 0)+ 
+  theme(legend.position = 'right',
+    legend.direction = "vertical",
+    axis.text = element_text(size=12, color="black"),  # 轴文本大小和颜色
+    axis.title = element_text(size=14, face="bold"),   # 轴标题大小和样式
+    plot.title = element_text(hjust = 0.5, size=16, face="bold")  ) +
+  coord_cartesian(ylim = c(0, 1)) +
+  labs(x='Hearing B', y="Consistency") +
+  scale_fill_manual(values = c('#f47c7c', '#f47c7c', '#f47c7c'))
+
+
+
+data <- read_excel("C:/Users/ASUS/Desktop/FollowUp.xlsx", col_names = TRUE, sheet = 2)
+
+
+data_condition <- data %>% select(ends_with("C1"))
+data_long <- data_condition %>%
+  pivot_longer(cols = everything(), names_to = "Condition", values_to = "value")
+ggplot(data_long, aes(x = factor(Condition), y = value, fill = Condition))+ 
+  stat_summary(fun = "mean",geom = "bar", width = 0.7, colour = "black", size = 0.5)+ 
+  geom_jitter(size = 2, width = 0.2)+ 
+  stat_summary(fun = "mean", fun.max = function(x) mean(x) + sd(x), fun.min = function(x) mean(x) - sd(x), 
+               geom = "errorbar", width = 0.4, size = 0.8)+ 
+  theme_prism(axis_text_angle = 0)+ 
+  theme(legend.position = 'right',
+    legend.direction = "vertical",
+    axis.text = element_text(size=12, color="black"),  # 轴文本大小和颜色
+    axis.title = element_text(size=14, face="bold"),   # 轴标题大小和样式
+    plot.title = element_text(hjust = 0.5, size=16, face="bold")  ) +
+  coord_cartesian(ylim = c(0, 0.2)) +
+  labs(x='Hearing each other', y="Stability") +
+  scale_fill_manual(values = c('#80d6ff', '#80d6ff', '#80d6ff'))
+
+data_condition <- data %>% select(ends_with("C2"))
+data_long <- data_condition %>%
+  pivot_longer(cols = everything(), names_to = "Condition", values_to = "value")
+ggplot(data_long, aes(x = factor(Condition), y = value, fill = Condition))+ 
+  stat_summary(fun = "mean",geom = "bar", width = 0.7, colour = "black", size = 0.5)+ 
+  geom_jitter(size = 2, width = 0.2)+ 
+  stat_summary(fun = "mean", fun.max = function(x) mean(x) + sd(x), fun.min = function(x) mean(x) - sd(x), 
+               geom = "errorbar", width = 0.4, size = 0.8)+ 
+  theme_prism(axis_text_angle = 0)+ 
+  theme(legend.position = 'right',
+    legend.direction = "vertical",
+    axis.text = element_text(size=12, color="black"),  # 轴文本大小和颜色
+    axis.title = element_text(size=14, face="bold"),   # 轴标题大小和样式
+    plot.title = element_text(hjust = 0.5, size=16, face="bold")  ) +
+  coord_cartesian(ylim = c(0, 0.2)) +
+  labs(x='Hearing A', y="Stability") +
+  scale_fill_manual(values = c('#80d6ff', '#80d6ff', '#80d6ff'))
+
+data_condition <- data %>% select(ends_with("C3"))
+data_long <- data_condition %>%
+  pivot_longer(cols = everything(), names_to = "Condition", values_to = "value")
+ggplot(data_long, aes(x = factor(Condition), y = value, fill = Condition))+ 
+  stat_summary(fun = "mean",geom = "bar", width = 0.7, colour = "black", size = 0.5)+ 
+  geom_jitter(size = 2, width = 0.2)+ 
+  stat_summary(fun = "mean", fun.max = function(x) mean(x) + sd(x), fun.min = function(x) mean(x) - sd(x), 
+               geom = "errorbar", width = 0.4, size = 0.8)+ 
+  theme_prism(axis_text_angle = 0)+ 
+  theme(legend.position = 'right',
+    legend.direction = "vertical",
+    axis.text = element_text(size=12, color="black"),  # 轴文本大小和颜色
+    axis.title = element_text(size=14, face="bold"),   # 轴标题大小和样式
+    plot.title = element_text(hjust = 0.5, size=16, face="bold")  ) +
+  coord_cartesian(ylim = c(0, 0.2)) +
+  labs(x='Hearing B', y="Stability") +
+  scale_fill_manual(values = c('#80d6ff', '#80d6ff', '#80d6ff'))
+

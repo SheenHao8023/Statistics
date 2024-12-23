e52d19e445a8fb764c4b42b791f7b51c62b0b791
Xin Hao <126965580+SheenHao8023@users.noreply.github.com>
2024-12-23 18:25:56 +0800

    Update Bar_Plot.r

--- Bar_Plot.r
+++ Bar_Plot.r
@@ -10,25 +10,26 @@ data <- data.frame(
   "A" = c(0.084745763,0.09375,0.075,0.136986301,0.065934066,0.090909091,0.095238095,0.12962963,0.125,0.053571429,0.055555556,0.070422535,0,0.125,0.053571429,0.055555556,0.070422535),
   "B" = c(0.06779661,0.0625,0.025,0.02739726,0.021978022,0.060606061,0.031746032,0.037037037,0.03125,0.035714286,0.111111111,0.112676056,0,0.03125,0.035714286,0.111111111,0.112676056),
   "C" = c(0.06779661,0.0625,0.025,0.02739726,0.021978022,0.060606061,0.031746032,0.074074074,0.125,0.071428571,0.111111111,0.084507042,0,0.125,0.071428571,0.111111111,0.084507042))
-data$Group <- c(rep("Positive", 8), rep("Negative", 4), rep('NA', 1), rep("Blunted Affect", 2), rep("Withdrawal", 2))
+data$Group <- c(rep("PS", 8), rep("NS", 4), rep('NA', 1), rep("BA", 2), rep("WD", 2))
 data_long <- data %>%
   pivot_longer(cols = -Group, names_to = "Metric", values_to = "Value")
 summary_data <- data_long %>%
   group_by(Metric, Group) %>%
   summarise(mean = mean(Value), sd = sd(Value), .groups = 'drop')
 levels(summary_data$Metric) <- c("A", "B", "C")
-summary_data$Group <- factor(summary_data$Group, levels = c("Positive", "Negative", 'NA', "Blunted Affect", "Withdrawal"))
-data_long$Group <- factor(data_long$Group, levels = c("Positive", "Negative", 'NA', "Blunted Affect", "Withdrawal"))
+summary_data$Group <- factor(summary_data$Group, levels = c('PS','NS','NA','BA','WD'))
+data_long$Group <- factor(data_long$Group, levels = c('PS','NS','NA','BA','WD'))
 summary_data$Metric <- factor(summary_data$Metric, levels = c("A", "B", "C"))
+
 ggplot(summary_data, aes(x = Group, y = mean, fill = Metric)) +
   geom_bar(stat = "identity", position = position_dodge(width = 0.9), width = 0.7, colour = NA, linewidth = 0) + 
   geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.4, position = position_dodge(width = 0.9), linewidth = 0.7) + 
   geom_jitter(data = subset(data_long, Group!= 'NA'), , aes(x = Group, y = Value, color = Metric), 
              position = position_jitterdodge(jitter.width = 0.1, dodge.width = 0.7), alpha = 1, size = 1, color = "black") + 
-  scale_fill_manual(values = c('#66c2a5','#fc8d62','#8da0cb', 'transparent', '#e78ac3','#a6d854'), name = "Items", labels = c("P1+P3", "N2+N4", "2N1")) + 
-  scale_x_discrete(breaks = c("Positive", "Negative", "Blunted Affect", "Withdrawal"), labels=c('PS', 'NS', 'BA', 'WD')) + 
+  scale_fill_manual(values = c('#fc8d62','#8da0cb','#6495ed'), name = "Items", labels = c("P1+P3", "N2+N4", "2N1")) + 
+  scale_x_discrete(breaks = c('PS','NS','BA','WD'), labels=c('PS', 'NS', 'BA', 'WD')) + 
   coord_cartesian(ylim = c(0, 0.15)) +
-  labs(x = element_blank(), y = "Relative Scores") +
+  labs(x = NULL, y = "Relative Scores") +
   theme_minimal() +
   theme(legend.direction = "vertical", legend.position = 'right',
     axis.line = element_line(color = 'black', size = 0.75),

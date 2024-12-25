6d1cf56bd76a2a2f42731d0b9e5ed9e4cead86d2
Xin Hao <126965580+SheenHao8023@users.noreply.github.com>
2024-12-25 16:31:17 +0800

    Update Bar_Plot.r

--- Bar_Plot.r
+++ Bar_Plot.r
@@ -5,7 +5,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 
-#除以总分
+#症状学结果
 data <- data.frame( 
   "A" = c(0.084745763,0.09375,0.075,0.136986301,0.065934066,0.090909091,0.095238095,0.12962963,0.125,0.053571429,0.055555556,0.070422535,0,0.125,0.053571429,0.055555556,0.070422535),
   "B" = c(0.06779661,0.0625,0.025,0.02739726,0.021978022,0.060606061,0.031746032,0.037037037,0.03125,0.035714286,0.111111111,0.112676056,0,0.03125,0.035714286,0.111111111,0.112676056),
@@ -20,25 +20,26 @@ levels(summary_data$Metric) <- c("A", "B", "C")
 summary_data$Group <- factor(summary_data$Group, levels = c('PS','NS','NA','BA','WD'))
 data_long$Group <- factor(data_long$Group, levels = c('PS','NS','NA','BA','WD'))
 summary_data$Metric <- factor(summary_data$Metric, levels = c("A", "B", "C"))
-
+summary_data$sd[is.na(summary_data$sd)] <- 0
 ggplot(summary_data, aes(x = Group, y = mean, fill = Metric)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.9), width = 0.7, colour = NA, linewidth = 0) + 
+  geom_bar(stat = "identity", position = position_dodge(width = 0.9), width = 0.7, colour = NA) + 
   geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.4, position = position_dodge(width = 0.9), linewidth = 0.7) + 
-  geom_jitter(data = subset(data_long, Group!= 'NA'), , aes(x = Group, y = Value, color = Metric), 
-             position = position_jitterdodge(jitter.width = 0.1, dodge.width = 0.7), alpha = 1, size = 1, color = "black") + 
+  geom_jitter(data = subset(data_long, Group != 'NA'), aes(x = Group, y = Value, color = Metric), 
+              position = position_jitterdodge(jitter.width = 0.1, dodge.width = 0.7), alpha = 1, size = 1) + 
   scale_fill_manual(values = c('#fc8d62','#8da0cb','#6495ed'), name = "Items", labels = c("P1+P3", "N2+N4", "2N1")) + 
-  scale_x_discrete(breaks = c('PS','NS','BA','WD'), labels=c('PS', 'NS', 'BA', 'WD')) + 
+  scale_color_manual(values = c('#fc8d62','#8da0cb','#6495ed'), name = "Items") + 
+  scale_x_discrete(breaks = c('PS','NS','BA','WD'), labels = c('PS', 'NS', 'BA', 'WD')) + 
   coord_cartesian(ylim = c(0, 0.15)) +
   labs(x = NULL, y = "Relative Scores") +
   theme_minimal() +
   theme(legend.direction = "vertical", legend.position = 'right',
-    axis.line = element_line(color = 'black', size = 0.75),
-    axis.ticks = element_line(color = 'black', size = 0.75),
-    axis.text = element_text(size=12, color="black", face='plain'),  
-    axis.title = element_text(size=14, face='plain'))
+        axis.line = element_line(color = 'black', size = 0.75),
+        axis.ticks = element_line(color = 'black', size = 0.75),
+        axis.text = element_text(size = 12, color = "black", face = 'plain'),  
+        axis.title = element_text(size = 14, face = 'plain'))
 
 
-#行为学结果图
+#行为学结果IC
 dat1 <- data.frame(group = factor(c(rep('HC',12),rep('PS',8),rep('NS',4),rep('NA',1),rep('BA',2),rep('WD',2)),
   levels = c('HC','PS','NS','NA','BA','WS')),
   exp = c(0.736740518696357, 0.718849142735551, 0.463212981412826, 0.814311576831261, 
@@ -64,7 +65,7 @@ ggplot(data = dat1, mapping = aes(x = factor(group), y = exp, fill = group))+
   scale_x_discrete(breaks = c('HC','PS','NS','BA','WS'), labels=c('HC','PS','NS','BA','WS'))+
   labs(x=NULL, y="IC")+
   geom_rect(aes(xmin=2.5, xmax=3.5,ymin=-0.02,ymax=0.4), fill = NA, color='black', linetype = 'dashed', size=0.75)
-
+#行为学结果WS
 dat2 <- data.frame(group = factor(c(rep('HC',12),rep('PS',8),rep('NS',4),rep('NA',1),rep('BA',2),rep('WD',2)),
   levels = c('HC','PS','NS','NA','BA','WS')),
   exp = c(0.202281279176387, 0.630912927170263, 0.493304878268097, 0.918978968429336, 

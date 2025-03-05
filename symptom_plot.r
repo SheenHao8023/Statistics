39714832f66afa9ee412b39985585c72c53baaad
Xin Hao <126965580+SheenHao8023@users.noreply.github.com>
2025-03-05 20:14:17 +0800

    Update symptom_plot.r

--- symptom_plot.r
+++ symptom_plot.r
@@ -12,23 +12,25 @@ long_data <- data %>%
   #  filter(Group == 4) %>% # ControlResting
   filter(ID == 'HZ002' & Type == 'SZ') %>% # Individual
   pivot_longer(cols = 9:ncol(data), names_to = "Measure", values_to = "Score")
-long_data$Time <- factor(long_data$Time, levels = c("Pre", "Post"))
+time_levels <- sort(unique(long_data$Time))
+long_data$Time <- factor(long_data$Time, levels = time_levels)
 theme_custom <- theme(
   panel.grid = element_blank(),
   axis.line = element_line(color = 'black', size = 0.75),
   axis.ticks = element_line(color = 'black', size = 0.75),
   axis.text = element_text(size = 12, color = "black"),
   axis.title = element_text(size = 14),
+  plot.title = element_text(size = 18, face = "bold"),
   legend.position = "none"
 )
-color_map <- c("Pre" = "#f47c7c", "Post" = "#80d6ff")
+color_map <- c("Pre" = "#f47c7c", "Post" = "#80d6ff", "FollowUp1" = "#92d050", "FollowUp2" = "#ffc000")
 measure_names <- unique(long_data$Measure) 
 
 for (measure in measure_names) {
   p <- ggplot(long_data %>% filter(Measure == measure), aes(x = Time, y = Score, fill = Time)) +
     geom_bar(stat = "summary", fun = "mean", width = 0.6, colour = "black", size = 0.5) +
     geom_errorbar(stat = "summary", fun.data = "mean_se", width = 0.2, size = 0.8) +
-    scale_fill_manual(values = color_map) +
+    scale_fill_manual(values = color_map[names(color_map) %in% time_levels]) +
     labs(x = "Time", y = "Score", title = measure) +
     theme_custom
   output_path <- paste0(dirname(file_path), "/", measure, "_bar_plot.png")
